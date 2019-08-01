34baeead66b9d2faa6d6baf3e59fad4a2f8885ad
lukasxsss <44241165+lukasxsss@users.noreply.github.com>
2019-08-01 02:04:42 +0200

    01.08

---
+++ blog/models.py
@@ -0,0 +1,22 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+STATUS = (
+    (0, "Edit"),
+    (1, "Public")
+)
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=100, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
+    content = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+
+    class Meta:
+        ordering = ['-created_on']
+
+    def __str__(self):
+        return self.title
---
+++ blog/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from django.views import generic
+
+from .models import Post
+
+
+class PostList(generic.ListView):
+    queryset = Post.objects.filter(status=1).order_by('-created_on')
+    template_name = 'home.html'
+
+
+class PostDetail(generic.DetailView):
+    model = Post
+    template_name = 'post_detail.html'
+
+
+def about(request):
+    return render(request, 'about.html')
